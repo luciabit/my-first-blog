668ba9f01b008ba505634e63a52a3686599a7dcd
Lucia Conti <lucia@unbit.it>
2016-04-17 11:38:34 +0200

    La mia app Django Girls, primo commit

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+	author = models.ForeignKey('auth.User') # link ad un altro modello
+	title = models.CharField(max_length=200) #testo con numero limitato di lettere
+	text = models.TextField() # testo senza limite
+	created_date = models.DateTimeField(default=timezone.now)
+	published_date = models.DateTimeField(blank=True, null=True) # non obbligatorio, può avere un valore nullo 
+
+def publish(self):
+	self.published_date = timezone.now()
+	self.save()
+
+def __str__(self):
+	return self.title
